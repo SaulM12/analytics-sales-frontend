5e676bc38dc9ba3f87fe4a283f678ea66f949c53
Mateo Pérez <futbolmate@gmail.com>
2022-06-12 17:46:04 -0500

    firts commit

---
+++ src/auth/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+function login() {
+    return (
+        <div>
+            <Link to="/register">fasdfas</Link>
+            <Button variant="outlined" href="/register">
+                fasfsdf
+            </Button>
+        </div>
+    )
+}
+
+export default login
