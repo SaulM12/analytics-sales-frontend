e5ffc3e90ec7561f180de2af37101c1a21c9fa77
Mauricio Espinoza <saulmme12@gmail.com>
2022-06-16 15:08:22 -0500

    Auth and home components

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -5,8 +5,19 @@ import BubbleChartSharpIcon from '@mui/icons-material/BubbleChartSharp';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+    const [userName,setUserName]=React.useState("")
+    const [password,setPassword]=React.useState("")
+    var navigate=useNavigate()
+    const verifyAuth=()=>{
+        if (userName===""||password==="") {
+            console.log(userName)
+        }else{
+           return navigate("/store",{replace:true})
+        }
+    }
     return (
         <div className={loginStyles.container}>
             <Stack spacing={2} className={loginStyles.card} alignItems="center" justifyContent="space-evenly">
@@ -17,12 +28,14 @@ function Login() {
                     </Typography>
                 </Stack>
                 <Stack spacing={2} alignItems="center" justifyContent="center">
-                    <TextField id="filled-basic" label="Usuario" variant="filled" />
-                    <TextField id="filled-basic" label="Contraseña" variant="filled" />
-                    <Button variant="contained" href="/store" className={loginStyles.button}>
+                    <TextField id="filled-basic" label="Usuario" variant="filled" value={userName} 
+                    onChange={value=>setUserName(value.target.value)} />
+                    <TextField id="filled-basic" label="Contraseña" variant="filled" value={password}
+                     onChange={value=>setPassword(value.target.value)}/>
+                    <Button variant="contained" className={loginStyles.button} onClick={verifyAuth}>
                         Iniciar Sesión
                     </Button>
-                    <Button variant="text" href="/register">
+                    <Button variant="text" href="/register" >
                         Registrarse
                     </Button>
                 </Stack>
